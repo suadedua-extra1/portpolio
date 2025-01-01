8a3f07ec350ab5b9756bb614a5beb7348554b1cd
SuaDeDua <sokvai123hn@gmail.com>
2025-01-01 12:22:09 +0700

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+    return (
+      <main>
+          <h1 className='text-2xl text-white underline'></h1>
+      </main>
+    )
+}
+export default App
