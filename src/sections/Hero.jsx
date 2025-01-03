a923e6e7684891464688546cd924ebec8047edcf
SuaDeDua <sokvai123hn@gmail.com>
2025-01-03 17:29:55 +0700

    Update header and something on hero

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,24 @@
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hi, am Long <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Building Products & Brands
+        </p>
+      </div>
+
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full">
+          <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+export default Hero;
